96120fa2e87ba02792266b998e2f30f9c9d48de8
Andrew Lee <lichundian@gmail.com>
2015-11-19 14:59:31 +0800

    Modify the token field name to a number

--- service.go
+++ service.go
@@ -18,21 +18,25 @@ const (
 	QUERY_ALL_ORDERS      = "/admin/orders"
 )
 
-var (
-	MALFORMED_JSON_MSG      = []byte("{\"code\": \"MALFORMED_JSON\",\"message\": \"格式错误\"}")
-	EMPTY_REQUEST_MSG       = []byte("{\"code\": \"EMPTY_REQUEST\",\"message\": \"请求体为空\"}")
-	USER_AUTH_FAIL_MSG      = []byte("{\"code\": \"INVALID_ACCESS_TOKEN\",\"message\": \"无效的令牌\"}")
-	CART_NOT_FOUND_MSG      = []byte("{\"code\": \"CART_NOT_FOUND\", \"message\": \"篮子不存在\"}")
-	NOT_AUTHORIZED_CART_MSG = []byte("{\"code\": \"NOT_AUTHORIZED_TO_ACCESS_CART\",\"message\": \"无权限访问指定的篮子\"}")
-	FOOD_OUT_OF_LIMIT_MSG   = []byte("{\"code\": \"FOOD_OUT_OF_LIMIT\",\"message\": \"篮子中食物数量超过了三个\"}")
-	FOOD_NOT_FOUND_MSG      = []byte("{\"code\": \"FOOD_NOT_FOUND\",\"message\": \"食物不存在\"}")
-	FOOD_OUT_OF_STOCK_MSG   = []byte("{\"code\": \"FOOD_OUT_OF_STOCK\", \"message\": \"食物库存不足\"}")
-	ORDER_OUT_OF_LIMIT_MSG  = []byte("{\"code\": \"ORDER_OUT_OF_LIMIT\",\"message\": \"每个用户只能下一单\"}")
+const (
+	TOTAL_FIELD = 0
 )
 
 // tuning parameters
 const (
 	CACHE_LEN = 70
+)
+
+var (
+	MALFORMED_JSON_MSG       = []byte("{\"code\": \"MALFORMED_JSON\",\"message\": \"格式错误\"}")
+	EMPTY_REQUEST_MSG        = []byte("{\"code\": \"EMPTY_REQUEST\",\"message\": \"请求体为空\"}")
+	INVALID_ACCESS_TOKEN_MSG = []byte("{\"code\": \"INVALID_ACCESS_TOKEN\",\"message\": \"无效的令牌\"}")
+	CART_NOT_FOUND_MSG       = []byte("{\"code\": \"CART_NOT_FOUND\", \"message\": \"篮子不存在\"}")
+	NOT_AUTHORIZED_CART_MSG  = []byte("{\"code\": \"NOT_AUTHORIZED_TO_ACCESS_CART\",\"message\": \"无权限访问指定的篮子\"}")
+	FOOD_OUT_OF_LIMIT_MSG    = []byte("{\"code\": \"FOOD_OUT_OF_LIMIT\",\"message\": \"篮子中食物数量超过了三个\"}")
+	FOOD_NOT_FOUND_MSG       = []byte("{\"code\": \"FOOD_NOT_FOUND\",\"message\": \"食物不存在\"}")
+	FOOD_OUT_OF_STOCK_MSG    = []byte("{\"code\": \"FOOD_OUT_OF_STOCK\", \"message\": \"食物库存不足\"}")
+	ORDER_OUT_OF_LIMIT_MSG   = []byte("{\"code\": \"ORDER_OUT_OF_LIMIT\",\"message\": \"每个用户只能下一单\"}")
 )
 
 var (
@@ -120,7 +124,7 @@ func createCart(writer http.ResponseWriter, req *http.Request) {
 	}
 	//fmt.Println(token)
 	cart_id, _ := redis.Int(rs.Do("INCR", "cart_id"))
-	rs.Do("HSET", "cart:"+strconv.Itoa(cart_id)+":"+token, "total", 0)
+	rs.Do("HSET", "cart:"+strconv.Itoa(cart_id)+":"+token, TOTAL_FIELD, 0)
 	rs.Close()
 
 	writer.WriteHeader(http.StatusOK)
@@ -128,8 +132,7 @@ func createCart(writer http.ResponseWriter, req *http.Request) {
 }
 
 func addFood(writer http.ResponseWriter, req *http.Request) {
-	// TODO
-	// Maybe the order in which two checking execute is important
+	// TODO Maybe the order in which two checking execute is important
 	rs := Pool.Get()
 	userExist, token := authorize(writer, req, rs)
 	if !userExist {
@@ -142,21 +145,17 @@ func addFood(writer http.ResponseWriter, req *http.Request) {
 		return
 	}
 	// transaction problem
-	cartStr := strings.Split(req.URL.Path, "/")[2]
-	cartId, _ := strconv.Atoi(cartStr)
+	cartIdStr := strings.Split(req.URL.Path, "/")[2]
+	cartId, _ := strconv.Atoi(cartIdStr)
 	cartIdMax, err := redis.Int(rs.Do("GET", "cart_id"))
-	fmt.Println("cartStr =", cartStr)
-	fmt.Println("cartIdMax =", cartIdMax)
-	fmt.Println("cartId =", cartId)
 	if err != nil || cartId > cartIdMax || cartId < 1 {
 		rs.Close()
 		writer.WriteHeader(http.StatusNotFound)
 		writer.Write(CART_NOT_FOUND_MSG)
 		return
 	}
-
-	cartKey := "cart:" + cartStr + ":" + string(token)
-	total, err := redis.Int(rs.Do("HGET", cartKey, "total"))
+	cartKey := "cart:" + cartIdStr + ":" + string(token)
+	total, err := redis.Int(rs.Do("HGET", cartKey, TOTAL_FIELD))
 	if err != nil {
 		rs.Close()
 		writer.WriteHeader(http.StatusUnauthorized)
@@ -194,6 +193,16 @@ func addFood(writer http.ResponseWriter, req *http.Request) {
 		writer.Write(FOOD_NOT_FOUND_MSG)
 		return
 	}
+
+	foodCount, foodErr := redis.Int(rs.Do("HGET", cartKey, item.FoodId))
+	fmt.Println("token =", token, "item.FoodId = ", item.FoodId, "item.Count = ", item.Count, "foodCount = ", foodCount)
+	if foodErr != nil {
+		rs.Do("HSET", cartKey, item.FoodId, item.Count)
+	} else {
+		// if item.Count+foodCount < 0, how to do?
+		rs.Do("HSET", cartKey, item.FoodId, item.Count+foodCount)
+	}
+
 	rs.Close()
 	writer.WriteHeader(http.StatusNoContent)
 	return
@@ -210,7 +219,47 @@ func orderProcess(writer http.ResponseWriter, req *http.Request) {
 }
 
 func submitOrder(writer http.ResponseWriter, req *http.Request) {
-	// TODO
+	rs := Pool.Get()
+	userExist, token := authorize(writer, req, rs)
+	if !userExist {
+		rs.Close()
+		return
+	}
+	isEmpty, body := checkBodyEmpty(writer, req)
+	if isEmpty {
+		rs.Close()
+		return
+	}
+	var cartIdJson CartIdJson
+	if err := json.Unmarshal(body, &cartIdJson); err != nil {
+		rs.Close()
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write(MALFORMED_JSON_MSG)
+		return
+	}
+	cartIdStr := cartIdJson.CartId
+
+	// transaction problem
+	// copy from the same code above
+	cartId, _ := strconv.Atoi(cartIdStr)
+	cartIdMax, err := redis.Int(rs.Do("GET", "cart_id"))
+	if err != nil || cartId > cartIdMax || cartId < 1 {
+		rs.Close()
+		writer.WriteHeader(http.StatusNotFound)
+		writer.Write(CART_NOT_FOUND_MSG)
+		return
+	}
+	cartKey := "cart:" + cartIdStr + ":" + string(token)
+	total, err := redis.Int(rs.Do("HGET", cartKey, TOTAL_FIELD))
+	if err != nil {
+		rs.Close()
+		writer.WriteHeader(http.StatusUnauthorized)
+		writer.Write(NOT_AUTHORIZED_CART_MSG)
+		return
+	}
+
+	// transaction problem
+	foodIdAndCounts, _ := redis.Ints(rs.Do("HGETALL"))
 
 	writer.Write([]byte("\nsubmit order"))
 }
@@ -233,9 +282,8 @@ func authorize(writer http.ResponseWriter, req *http.Request, rs redis.Conn) (bo
 		token = req.Form.Get("access_token")
 	}
 	if exist, _ := redis.Bool(rs.Do("SISMEMBER", "tokens", token)); !exist {
-		writer.WriteHeader(http.StatusForbidden)
-		writer.Write(USER_AUTH_FAIL_MSG)
-		fmt.Println("zheer")
+		writer.WriteHeader(http.StatusUnauthorized)
+		writer.Write(INVALID_ACCESS_TOKEN_MSG)
 		return false, ""
 	}
 	return true, token
